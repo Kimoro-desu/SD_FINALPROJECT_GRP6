dcbfeaed288677a2e380b2a64aab39970962eed8
uno migz <1migz.pc@gmail.com>
2026-04-25 22:01:19 -0700

    Delete HariBorrow_frontend directory

--- HariBorrow_frontend/js/auth_guard.js
+++
@@ -1,9 +0,0 @@
-// js/auth_guard.js
-// Include this script in the <head> of any protected page.
-(function() {
-    const token = localStorage.getItem('jwt');
-    if (!token) {
-        // Fallback redirection to login if no token is found in localStorage
-        window.location.href = 'login.html';
-    }
-})();
